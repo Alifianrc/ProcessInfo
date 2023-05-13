20757a70e6b2f1439ff81018e29ac0bc8a5529dc
Alifianrc <62532983+Alifianrc@users.noreply.github.com>
2023-05-13 16:12:15 +0700

    First Commit

---
+++ ProcessInfo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Get a list of all running processes
+        Process[] processes = Process.GetProcesses();
+
+        // Iterate through each "dotnet run" process and display its name and CPU usage
+        foreach (Process process in processes)
+        {
+            Console.WriteLine("{0} - CPU Usage: {1}%", process.ProcessName, process.TotalProcessorTime.Milliseconds);
+        }
+    }
+}
