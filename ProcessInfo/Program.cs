b396fe19d743735b397889df9957fb1159ded953
Alifianrc <62532983+Alifianrc@users.noreply.github.com>
2023-05-13 16:25:32 +0700

    Update Program.cs

--- ProcessInfo/Program.cs
+++ ProcessInfo/Program.cs
@@ -6,13 +6,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Get a list of all running processes
-        Process[] processes = Process.GetProcesses();
-
-        // Iterate through each "dotnet run" process and display its name and CPU usage
-        foreach (Process process in processes)
+        while (true)
         {
-            Console.WriteLine("{0} - CPU Usage: {1}%", process.ProcessName, process.TotalProcessorTime.Milliseconds);
+            try
+            {
+                // Get the process ID for the process you want to monitor
+                int processId = Process.GetProcessesByName("Server")[0].Id;
+
+                // Get the process object for the specified process ID
+                Process process = Process.GetProcessById(processId);
+
+                // Get the total CPU time for the process
+                TimeSpan totalCpuTime = process.TotalProcessorTime;
+
+                // Get the amount of time that the process has been running
+                TimeSpan runningTime = DateTime.Now - process.StartTime;
+
+                // Calculate the CPU usage as a percentage
+                double cpuUsage = (totalCpuTime.TotalMilliseconds / runningTime.TotalMilliseconds) * 100;
+
+                // Display the CPU usage percentage
+                Console.WriteLine("CPU Usage: {0}%", cpuUsage);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.Clear();
+            Thread.Sleep(1000);
         }
     }
 }
